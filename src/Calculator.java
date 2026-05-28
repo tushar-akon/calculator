a7a723bb0e5465c813f08f67f506787c8bf2f699
tushar-akon <tusharakon1617@gmail.com>
2026-05-28 12:30:28 +0900

    added pannel

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,12 +9,19 @@ public class Calculator {
     int windowWidth = 500;
     int windowHeight = 700;
 
-    
+    //colors used in this app//
     Color lightGray =new Color(212,212,210);
     Color darkGray =new Color(80,80,80);
+    Color Black = new Color(28,28,28);
+    Color orange = new Color(255,149,0);
+
     
 
-    JFrame calFrame = new JFrame("calculator ");
+    JFrame calFrame = new JFrame("Lets Do Some Math ");
+    JLabel displayLabel= new JLabel();
+    JPanel displayPanel = new JPanel();
+
+
 
     //Pop up window size and controls//
     Calculator(){
@@ -24,6 +31,21 @@ public class Calculator {
         calFrame.setResizable(false);
         calFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calFrame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(Black);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial",Font.PLAIN,80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayPanel);
+        
+
+
+
 
     }
 
